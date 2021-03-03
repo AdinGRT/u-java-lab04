3b0ace1a0825adb9061c4cc1a74aaa6cf13e17df
AdinGRT <adingrt@gmail.com>
2021-03-02 18:00:17 -0600

    Se agrego el metodo editarCliente al ServletControlador y se modifico el metodo doGet para procesar distintas acciones

--- src/main/java/web/ServletControlador.java
+++ src/main/java/web/ServletControlador.java
@@ -18,7 +18,19 @@ public class ServletControlador extends HttpServlet {
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        this.accionDefault(request, response);
+        String accion = request.getParameter("accion");
+        if(accion != null) {
+            switch(accion) {
+                case "editar":
+                    this.editarCliente(request, response);
+                    break;
+                default:
+                    this.accionDefault(request, response);
+            }
+        }
+        else {
+            this.accionDefault(request, response);
+        }
     }
     
     private void accionDefault(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -46,6 +58,15 @@ public class ServletControlador extends HttpServlet {
             saldoTotal += cliente.getSaldo();
         }
         return saldoTotal;
+    }
+    
+    private void editarCliente(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Recuperamos el idCliente
+        int idCliente = Integer.parseInt(request.getParameter("idCliente"));
+        Cliente cliente = new ClienteDaoJDBC().encontrar(new Cliente(idCliente));
+        request.setAttribute("cliente", cliente);
+        String jspEditar = "/WEB-INF/paginas/cliente/editarCliente.jsp";
+        request.getRequestDispatcher(jspEditar).forward(request, response);
     }
     
     @Override
